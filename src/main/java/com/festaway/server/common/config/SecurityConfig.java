f52df631e0476edb30713962ba444ca5b77eb449
cheonHwi <hoba0409@naver.com>
2025-04-16 17:13:51 +0900

    feature : spring security 기본 세팅

---
+++ src/main/java/com/festaway/server/common/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.festaway.server.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CorsConfigurationSource corsConfigurationSource;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->  authorize
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
