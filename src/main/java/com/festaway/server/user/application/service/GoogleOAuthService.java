3fdcf78db4ff9f0d82577000c592729f27d6157f
cheonHwi <hoba0409@naver.com>
2025-05-12 11:23:13 +0900

    refactor : 사용하지 않는 import문 제거

--- src/main/java/com/festaway/server/user/application/service/GoogleOAuthService.java
+++ src/main/java/com/festaway/server/user/application/service/GoogleOAuthService.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
